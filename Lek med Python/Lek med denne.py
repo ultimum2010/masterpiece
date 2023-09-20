0cddfdf50e139166f0d0a042e9e89baafd7ed3d3
Sivert S Johannessen <sivertsj10@gmail.com>
2023-09-20 21:57:05 +0200

    første commit

---
+++ Lek med Python/Lek med denne.py
@@ -0,0 +1,16 @@
+# Denne filen er for å leke med Python og Git.
+# Her tester vi hva som helst og commit'er det
+# til repositoryet vårt.
+
+alma = "Alma"
+ingeborg = "Ingeborg"
+sivert = "Sivert"
+kasper = "Kasper"
+hello = "Hello World"
+tolv = 9
+
+#Dette er en klassiker i programmering
+print(hello)
+
+#Denne gir kanskje ikke helt det resultatet man skulle forvente?
+print(alma, "er", tolv+1, "år")
