fe9d190ceb04edddcea4754b533caf35ab410e13
sivertsj10 <92299801+sivertsj10@users.noreply.github.com>
2023-10-28 15:16:17 +0200

    har fortsettet på oppdrag 14 med 40 p. får ikke til å dispense publikum

--- Teknologi/RobotProgram/oppdrag 14-40p.py
+++ Teknologi/RobotProgram/oppdrag 14-40p.py
@@ -6,9 +6,49 @@ chewie = Chewbacca()
 
 #kjøring til mål
 chewie.drive_gyro_dist(100, 0, 100)
-chewie.drive_gyro_dist(100, 22, 620)
+chewie.drive_gyro_dist(200, 22, 620)
 
 #dispensing av publikum
-chewie.work_motor_L(70, 700)
+chewie.work_motor_L(100, 700)
+chewie.work_motor_R(40, 100)
+chewie.work_motor_R(30, -120)
+chewie.work_motor_R(40, 100)
+chewie.work_motor_R(30, -120)
+chewie.beep()
+
+chewie.work_motor_L(100, 400)
 chewie.work_motor_R(30, 100)
-chewie.work_motor_R(30, -100)
+chewie.work_motor_R(30, -120)
+chewie.work_motor_R(40, 100)
+chewie.work_motor_R(30, -120)
+chewie.beep()
+
+chewie.work_motor_L(100, 400)
+chewie.work_motor_R(30, 100)
+chewie.work_motor_R(30, -120)
+chewie.work_motor_R(40, 100)
+chewie.work_motor_R(30, -120)
+
+chewie.work_motor_L(100, 400)
+chewie.work_motor_R(30, 100)
+chewie.work_motor_R(30, -120)
+chewie.work_motor_R(40, 100)
+chewie.work_motor_R(30, -120)
+
+chewie.work_motor_L(100, 400)
+chewie.work_motor_R(30, 100)
+chewie.work_motor_R(30, -120)
+chewie.work_motor_R(40, 100)
+chewie.work_motor_R(30, -120)
+
+chewie.work_motor_L(100, 400)
+chewie.work_motor_R(30, 100)
+chewie.work_motor_R(30, -120)
+chewie.work_motor_R(40, 100)
+chewie.work_motor_R(30, -120)
+
+chewie.work_motor_L(100, 400)
+chewie.work_motor_R(30, 100)
+chewie.work_motor_R(30, -120)
+chewie.work_motor_R(40, 100)
+chewie.work_motor_R(30, -120)
