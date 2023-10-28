2c4d0d25bbaf710a47e2caaae8d35b8687e021cd
ultimum2010 <145459288+ultimum2010@users.noreply.github.com>
2023-10-28 14:31:39 +0200

    Roboten går frem tl oppdrag 7, men den vil ikke gå tilbake

--- Teknologi/RobotProgram/oppdrag 7.py
+++ Teknologi/RobotProgram/oppdrag 7.py
@@ -5,4 +5,8 @@ from chewbacca import Direction
 chewie = Chewbacca()
 
 #kjøring til vendepongt
-chewie.drive_gyro_dist(100, 10, 700)
+chewie.drive_gyro_dist(200, 20, 750)
+#Vi prøver å få roboten til å gå tilbake, men den vil ikke gjøre det. 
+#vi hører beepet:)  
+chewie.drive_gyro_dist(-600, 20, -750)
+chewie.beep()
