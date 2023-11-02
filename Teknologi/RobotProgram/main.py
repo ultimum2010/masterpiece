37affcb21d65c21e15b9e5d54a6de66faf1b644b
sivertsj10 <92299801+sivertsj10@users.noreply.github.com>
2023-11-02 21:38:34 +0100

    har begynt på masterprogram

--- Teknologi/RobotProgram/main.py
+++ Teknologi/RobotProgram/main.py
@@ -1,11 +1,41 @@
 #!/usr/bin/env pybricks-micropython
 from chewbacca import Chewbacca
+from pybricks.tools import wait
 
 chewie = Chewbacca()
 
-chewie.beep()
+# init variabler her
+prognr = 4
 
-# chewie.drive_gyro_dist(100, 0, 2100)
 
-chewie.drive_gyro_dist(100, 90, 100)
-#chewie.work_motor_R(10, 10)
+#oppdater skjerm
+if prognr == 0:
+    chewie.brain.screen.load_image("/home/robot/RobotProgram/chewbacca/menybilder/0.png")
+elif prognr == 1:
+    chewie.brain.screen.load_image("/home/robot/RobotProgram/chewbacca/menybilder/1.png")
+elif prognr == 2:
+    chewie.brain.screen.load_image("/home/robot/RobotProgram/chewbacca/menybilder/2.png")
+elif prognr == 3:
+    chewie.brain.screen.load_image("/home/robot/RobotProgram/chewbacca/menybilder/3.png")
+elif prognr == 4:
+    chewie.brain.screen.load_image("/home/robot/RobotProgram/chewbacca/menybilder/4.png")
+elif prognr == 5:
+    chewie.brain.screen.load_image("/home/robot/RobotProgram/chewbacca/menybilder/5.png")
+elif prognr == 6:
+    chewie.brain.screen.load_image("/home/robot/RobotProgram/chewbacca/menybilder/6.png")
+elif prognr == 7:
+    chewie.brain.screen.load_image("/home/robot/RobotProgram/chewbacca/menybilder/7.png")
+elif prognr == 8:
+    chewie.brain.screen.load_image("/home/robot/RobotProgram/chewbacca/menybilder/8.png")
+elif prognr == 9:
+    chewie.brain.screen.load_image("/home/robot/RobotProgram/chewbacca/menybilder/9.png")
+
+
+#les knapper
+knapp_er_trykt = False
+while not knapp_er_trykt:
+    knapp_verdi = chewie.brain.buttons.pressed() 
+
+    #er alle knapper ute
+    if knapp_verdi:
+        knapp_er_trykt = True
