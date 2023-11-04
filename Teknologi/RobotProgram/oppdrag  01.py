158de756bd92a2435a1cdecec54e56372ebb1fe5
sivertsj10 <92299801+sivertsj10@users.noreply.github.com>
2023-11-04 15:45:38 +0100

    Byttet navn på programmer

--- Teknologi/RobotProgram/oppdrag  01.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env pybricks-micropython
-from chewbacca import Chewbacca
-from chewbacca import Direction
-
-chewie = Chewbacca()
-#kjør til instalasjon
-chewie.drive_gyro_dist (200, 25, 370)
-#robbot svinger men stopper ikke.
-chewie.vri_grader(200, 45)
