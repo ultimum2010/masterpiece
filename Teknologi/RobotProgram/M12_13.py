5b4bd76e201f54c0abd8df616bc045d310bb49dd
Kasper Borgan <Kasperborgan5@gmail.com>
2023-11-06 19:19:55 +0100

    har programmert motoren til verktøyet

--- Teknologi/RobotProgram/M12_13.py
+++ Teknologi/RobotProgram/M12_13.py
@@ -11,6 +11,7 @@ def run(en_Chewbacca_som_kommer_fra_utsiden):
     chewie.gyro.reset_angle(0)
     chewie.motor_R.run_angle(70, 130)
     chewie.drive_gyro_dist(100, 50, 470)
+    # her måtte det programmeres slik at tannhjulet treffer med sikkerhet
     chewie.motor_L.run_angle(40, 70)
     # nå skal roboten kjøre tannhjulet rundt ved bruk av høyre motor
     chewie.__motor_work_R__.run_angle(1500, 1600)
