99f3994cadcd039b4572bd3112f1866a274130c8
sivertsj10 <92299801+sivertsj10@users.noreply.github.com>
2023-11-09 22:03:47 +0100

    begynt på flyting til andre siden men gjør det lettere

--- Teknologi/RobotProgram/M08.py
+++ Teknologi/RobotProgram/M08.py
@@ -32,17 +32,25 @@ def run1(en_Chewbacca_som_kommer_fra_utsiden):
     startprepos()
 
     #dytt ned sperren på kameraskinnen
-    chewie.drive_gyro_dist(100, 0, 50)
-    chewie.vri_grader(-100)
+    chewie.drive_gyro_dist(100, 0, 52)
+    chewie.vri_grader(-110)
     chewie.drive_gyro_dist(200, -95, 100)
-    chewie.drive_gyro_dist(200, -90, 300)
+    chewie.drive_gyro_dist(200, -91, 350)
+
+    chewie.vri_grader(20)
+    chewie.vri_grader(-19)
 
     endprepos()
 
-    chewie.drive_gyro_dist(50, -88, 100)
+    chewie.drive_gyro_dist(50, -86, 100)
     chewie.drive_gyro_dist(50, -90, 40)
+    chewie.vri_grader(5)
     chewie.drive_gyro_dist(300, -90, -300)
 
     #hent ekspert
-    chewie.vri_grader(80)
-    chewie.drive_gyro_dist(200, -60 ,100)
+    chewie.vri_grader(70)
+    chewie.drive_gyro_dist(200, -56 ,320)
+    chewie.vri_grader(25)
+    chewie.drive_gyro_dist(50, -57, 10)
+    chewie.drive_gyro_dist(100, -110, 200)
+    chewie.drive_gyro_dist(500, -90, 900)
